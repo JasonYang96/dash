15a0005caf8d1d1938a0ab14667342a83976c8bf
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-18 02:17:04 +0000

    Deploy JasonYang96/dash to github.com/JasonYang96/dash.git:gh-pages

---
+++ precache-manifest.18d14ca7f94ef5d341c4ee029ef28bf9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0816bef41580791695f421110439f9f2",
+    "url": "/dash/index.html"
+  },
+  {
+    "revision": "8df8ae67bc153b8103c1",
+    "url": "/dash/static/css/main.9efb35e9.chunk.css"
+  },
+  {
+    "revision": "45ec16dbdd61ee3dc65b",
+    "url": "/dash/static/js/2.3e4633bf.chunk.js"
+  },
+  {
+    "revision": "8df8ae67bc153b8103c1",
+    "url": "/dash/static/js/main.cb135c29.chunk.js"
+  },
+  {
+    "revision": "a80bbb6662ddc1b53257",
+    "url": "/dash/static/js/runtime-main.76b617f2.js"
+  }
+]);
